732f31cea162dcb2e3d7418aebcaec8bb1ceea75
Ryan <ryancm1170@gmail.com>
2024-01-12 18:17:24 -0500

    add email section

---
+++ src/app/api/send/route.tsx
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  if (req.method !== "POST") {
+    res.status(405).end();
+    return;
+  }
+
+  try {
+    const { email, subject, message } = req.body;
+    console.log(email, subject, message);
+
+    const data = await resend.emails.send({
+      from: fromEmail,
+      to: [fromEmail, email],
+      subject: subject,
+      react: (
+        <>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
+        </>
+      ),
+    });
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+}
--- src/app/components/ProjectTag.tsx
+++ src/app/components/ProjectTag.tsx
@@ -1,15 +1,20 @@
-import { FC } from "react";
+import React from "react";
 
 interface ProjectTagProps {
   name: string;
-  isSelected: boolean;
   onClick: (tagName: string) => void;
+  isSelected: boolean;
 }
 
-const ProjectTag: FC<ProjectTagProps> = ({ name, onClick, isSelected }) => {
+const ProjectTag: React.FC<ProjectTagProps> = ({
+  name,
+  onClick,
+  isSelected,
+}) => {
   const buttonStyles = isSelected
     ? "text-white border-primary-500"
     : "text-[#ADB7BE] border-slate-600 hover:border-white";
+
   return (
     <button
       className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
