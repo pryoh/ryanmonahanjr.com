c5a879293d8810a00989d7206419500a0996a6a1
Ryan <ryancm1170@gmail.com>
2024-01-24 13:23:59 -0500

    remove route.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import AchievementsSection from "./components/AchievementSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
-import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import Languages from "./components/Languages";
 import Technologies from "./components/Technologies";
